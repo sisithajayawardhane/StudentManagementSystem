857fdd6bfdb7bf9dce92313b6aa250f1a6ba8465
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-01-18 17:39:49 +0530

    backend tested

--- Backend/routes/students.js
+++ Backend/routes/students.js
@@ -44,7 +44,7 @@ router.route("/update/:id").put(async (req,res) => {
     }
 
     const update = await Student.findByIdAndUpdate(userId, updateStudent).then(()=>{
-        res.status(200).send({status: "User updated", user: update});
+        res.status(200).send({status: "User updated"});
     }).catch((err)=>{
         console.log(err);
         res.status(500).send({status: "Error with updating data", error: error.message});
@@ -63,10 +63,12 @@ router.route("/delete/:id").delete(async (req,res) => {
     })
 })
 
+//Get one user details route
+
 router.route("/get/:id").get(async (req,res) => {
     let userId = req.params.id;
-    await Student.findById(userId).then(()=>{
-        res.status(200).send({status:"User fetched", user: user});
+    const user =  await Student.findById(userId).then((student)=>{
+        res.status(200).send({status:"User fetched",student});
     }).catch((err)=>{
         console.log(err.message);
         res.status(500).send({status:"Error with get user", error: err.message});
